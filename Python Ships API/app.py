f059d6abad110f122a4d7b1e1ff388ba05b8d07b
Cole L Simchick <clsimchick@gmail.com>
2018-10-05 14:18:48 -0400

    Continued making changes, added vagrant to the gitignore

--- Python Ships API/app.py
+++ Python Ships API/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'index'
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'apidb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+@app.route('/ships', methods=['GET'])
+def allShips():
+    query = "SELECT name FROM ships"
+    ships = cursor.execute(query)
+    return jsonify(str(ships))
