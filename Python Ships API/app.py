65f9ae0153ce79de0469911c1e3fd98db0b79fc6
Cole L Simchick <clsimchick@gmail.com>
2018-10-09 20:49:01 -0400

    Completed "get all" route and created a "get one" route

--- Python Ships API/app.py
+++ Python Ships API/app.py
@@ -16,6 +16,11 @@ cursor = conn.cursor()
 
 @app.route('/ships', methods=['GET'])
 def allShips():
-    query = "SELECT name FROM ships"
-    ships = cursor.execute(query)
-    return jsonify(str(ships))
+    query = cursor.execute("SELECT * FROM ships")
+    return jsonify(cursor.fetchall())
+
+@app.route('/ships/<int:ship_id>', methods=['GET'])
+def oneShip(ship_id):
+    id = ship_id
+    query = cursor.execute("SELECT * FROM ships WHERE id=" + str(id))
+    return jsonify(cursor.fetchone())
