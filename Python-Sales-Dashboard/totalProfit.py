3ffcba541bc2004b21b5f45e7b5d0a151a766d04
Cole L Simchick <clsimchick@gmail.com>
2018-10-23 09:37:29 -0400

    Continued work on the Python-Sales-Dashboard. Removed Rails-blog file

--- Python-Sales-Dashboard/totalProfit.py
+++ Python-Sales-Dashboard/totalProfit.py
@@ -14,32 +14,60 @@ with open('sales_staff.json') as staffFile:
     staff = json.loads(staffFile.read())
 
 
-total = []
-base = []
-acc = []
-tax = []
+# Total Profit
 
-for x in sales:
-    base.append(float(x["base_price"][1:]))
-    acc.append(float(x["accessories"][1:]))
-    tax.append(float(x["taxes"][1:]))
-base_sum = sum(base)
-acc_sum = sum(acc)
-tax_sum = sum(tax)
-total = base_sum + acc_sum - tax_sum
+# total = []
+# base = []
+# acc = []
+# tax = []
+#
+# for x in sales:
+#     base.append(float(x["base_price"][1:]))
+#     acc.append(float(x["accessories"][1:]))
+#     tax.append(float(x["taxes"][1:]))
+# base_sum = sum(base)
+# acc_sum = sum(acc)
+# tax_sum = sum(tax)
+# total = base_sum + acc_sum - tax_sum
+#
+# print(total)
+#
+# # Total vehicles sold per manufacturer
+#
+# make = {}
+#
+# for x in range(len(sales)):
+#     make[sales[x]["make"]] = 0
+#
+# for x in range(len(sales)):
+#     make[sales[x]["make"]] += 1
+#
+# print(make)
+#
+# fig, ax = plt.subplots()
+# y_pos = range(len(make))
+# values = (list(make.values()))
+#
+# ax.barh(y_pos, values, align="center")
+# plt.yticks(range(len(make)), list(make.keys()))
+# plt.ylabel("Manufacturer")
+# plt.xlabel("Total Vehicles Sold")
+# plt.show()
 
-print(total)
+# Top 10 cars sold
 
-make = {}
+mm = {}
 
 for x in range(len(sales)):
-    make[sales[x]["make"]] = 0
+    make_model = ""
+    make_model = sales[x]["make"] + "_" + sales[x]["model"]
 
-for x in range(len(sales)):
-    make[sales[x]["make"]] += 1
+    if make_model in mm:
+        mm[make_model] += 1
 
-print(make)
+    else:
+        mm[make_model] = 1
 
-plt.bar(make, height=15, color='#CE0404')
-plt.xticks(rotation=45, ha="right")
-plt.show()
+list_mm = list(mm.values())
+print(list_mm)
+print(list_mm.sort())
